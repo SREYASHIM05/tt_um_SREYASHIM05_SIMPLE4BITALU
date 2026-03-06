02f1f98f2eb21948980c7170545ae8f407dbee29
Sreyashi-Mandal_4148 <2404148@kiit.ac.in>
2026-03-06 12:06:48 +0530

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
+  tt_um_SREYASHIM05_SIMPLE4BITALU (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
